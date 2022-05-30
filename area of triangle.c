f247504a99f53b144a1392e177fa28d73ddbc6f2
KonkaPremanjali <105779683+KonkaPremanjali@users.noreply.github.com>
2022-05-30 22:46:21 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int base,height,area;
+    printf("enter base value:");
+    scanf("%d",&base);
+    printf("enter height value:");
+    scanf("%d",&height);
+    area=(base*height)/2;
+    printf("area of traingle:%d",area);
+
+    return 0;
+}
